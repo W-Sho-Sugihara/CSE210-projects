e8709057f4ad55aa5fdb5ac7d9089568c5cb33e3
W-Sho-Sugihara <shosmail31@gmail.com>
2026-06-13 17:13:10 +0900

    additional stuff

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,6 @@
+// added a user score history option to see progress with dates and scores
+// each goal is saves in its own goal type txt file for easier filtering and separating file loading functionality
+
 using System;
 
 class Program
